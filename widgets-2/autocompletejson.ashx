5f278212c14c0ace9841469306828cf11c85e3ba
Ximena <yayitax_cadena95@hotmail.com>
2019-12-16 16:52:53 -0700

    Finished Module widgets 2

---
+++ widgets-2/autocompletejson.ashx
@@ -0,0 +1,47 @@
+﻿<%@ WebHandler Language="C#" Class="Autocompletejson" %>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+public class Autocompletejson : IHttpHandler {
+    
+    public void ProcessRequest (HttpContext context) {
+        context.Response.ContentType = "application/json";
+
+        var term = context.Request["term"] ?? "";
+        var matches = from c in _classes
+                      where c.Value.ToLower().StartsWith(term.ToLower())
+                      select new
+                                 {
+                                    value = c.Key, 
+                                    label = c.Value
+                                 };
+
+        var serializer = new JavaScriptSerializer();
+        context.Response.Write(serializer.Serialize(matches));
+    }
+ 
+    public bool IsReusable {
+        get {
+            return false;
+        }
+    }
+
+    private IDictionary<int, string> _classes = new Dictionary<int, string> {
+                                    {  0, "Remote" },
+                                    {  1, "C#" },
+                                    {  2, "Entity Framework" },
+                                    {  3, "LINQ" },
+                                    {  4, "ASP.NET MVC" },
+                                    {  5, "jQuery UI" },
+                                    {  6, "CSS" },
+                                    {  7, "Ruby" },
+                                    {  8, "jQuery" },
+                                    {  9, "JavaScript" },
+                                    { 10, "Test driven development" },
+                                    { 11, "Design patterns" } 
+                                };
+}
--- widgets-2/scripts/page.js
+++ widgets-2/scripts/page.js
@@ -1,26 +1,24 @@
 ﻿/// <reference path="jquery-1.6.2.js" />
 /// <reference path="jquery-ui-1.8.16.custom.js" />
 
-$(function () {
+$(function() {
 
-    $("#dialog").dialog(
-        {
-            autoOpen: false,
-            modal: true,
-            resizable: false,
-            draggable: false,
-            position: "top",
-            buttons: {
-                "Yes, I do!": function () {
-                    $("#dialog").dialog("close");
-                    $("#dialogDecision").text("Yes you did!!");
-                },
-                "No": function () { }
-            }
+    $("#dialog").dialog({
+        autoOpen: false,
+        modal: true,
+        resizable: false,
+        draggable: false,
+        position: "top",
+        buttons: {
+            "Yes, I do!": function() {
+                $("#dialog").dialog("close");
+                $("#dialogDecision").text("Yes you did!!");
+            },
+            "No": function() {}
         }
-     );
+    });
 
-    $("#openDialog").click(function () {
+    $("#openDialog").click(function() {
         $("#dialog").dialog("open");
     });
 
@@ -39,20 +37,73 @@ $(function () {
 
         if (value > 0) {
             setTimeout(countdown, 100);
-        }
-        else {
+        } else {
             $("#countdown").text("completed");
             $("#progress").progressbar("disable");
         }
     }
 
-    $("#slider").slider({ min: 0, max: 100, value: 100, slide: function (event, ui) {
-        $("#slider").prev().css({ opacity: ui.value / 100 });
-    }
+    $("#slider").slider({
+        min: 0,
+        max: 100,
+        value: 100,
+        slide: function(event, ui) {
+            $("#slider").prev().css({ opacity: ui.value / 100 });
+        }
     });
 
     $("#tabs").tabs();
 
-    
+    $("#accordion").accordion({
+        autoHeight: false,
+        collapsible: true,
+        change: function(event, ui) {
+            console.log(event);
+            console.log(ui);
+        }
+    });
+
+    $("#accordion").accordion("activate", false);
+
+    var classes = [
+        "C#",
+        "Entity Framework",
+        "LINQ",
+        "ASP.NET MVC",
+        "jQuery UI",
+        "CSS",
+        "Ruby",
+        "jQuery",
+        "JavaScript",
+        "Test driven development",
+        "Design patterns"
+    ];
+
+    $("#search").autocomplete({
+        source: "autocompletejson.ashx",
+        minLength: 2,
+        delay: 1500
+    });
+
+    $("#buttons").children()
+        .button({ icons: { primary: "ui-icon-search", secondary: "ui-icon-wrench" } })
+        .click(function() {
+            alert($(this).value);
+        });
+
+    $("#radios").buttonset();
+    $("#checks").buttonset();
+
+    $("#dateselection").datepicker({
+        showAnim: "bounce",
+        numberOfMonths: 2,
+        showWeek: true,
+        changeMonth: true,
+        changeYear: true,
+        showButtonPanel: true,
+        minDate: new Date(2008, 1 - 1, 1),
+        maxDate: new Date(2012, 12 - 1, 31)
+    });
+    $("#dateselection").datepicker("setDate", new Date(2011, 6 - 1, 11));
 
 });
