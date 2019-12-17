8c2a02d6f10adfd2a5ed998b47bb19ad47437c25
Ximena <yayitax_cadena95@hotmail.com>
2019-12-16 17:16:12 -0700

    Finished Module Interactions

--- interactions/scripts/page.js
+++ interactions/scripts/page.js
@@ -1,3 +1,29 @@
 ﻿/// <reference path="jquery-1.6.2.js" />
 /// <reference path="jquery-ui-1.8.16.custom.js" />
 
+$(function() {
+    $("#d1").draggable({ snap: "#d2 #d3", revert: "invalid" });
+    $("#d2").draggable({
+        grid: [50, 100],
+        helper: function() {
+            return $("<div>I am a helper</div>")
+        }
+    });
+    $("#d3").draggable({ handle: "header" });
+
+    $("#d1 #d2 #d3").draggable("option", "stack", ".ui-draggable");
+
+    $("#trash").droppable({
+        activeClass: "opaque",
+        accept: "#d3",
+        drop: function(event, ui) {
+            ui.draggable.fadeOut(function() {
+                ui.draggable.remove();
+            });
+        }
+    });
+
+    $("#sortable").sortable({ placeholder: "placeholder" });
+
+    $("#d3").resizable({ aspectRatio: true, alsoResize: "#d2" });
+});
