45d3e7c26c43d039086bc97fd99b9d0fbf8f3b7c
Ximena <yayitax_cadena95@hotmail.com>
2019-12-16 17:47:22 -0700

    Finished Module Effects

--- effects/scripts/page.js
+++ effects/scripts/page.js
@@ -1,6 +1,33 @@
 ﻿/// <reference path="jquery-1.6.2.js" />
 /// <reference path="jquery-ui-1.8.16.custom.js" />
 
-$(function () {
-    
+$(function() {
+    $("#target")
+        .css("position", "relative")
+        .click(function() {
+            // $(this).animate({
+            //     "width": "+=20",
+            //     "height": "+=20",
+            //     "background-color": "red"
+            // });
+
+            // $(this).effect("explode", { pieces: 16 }, 2000, function() {
+            //     alert("Done-zo!");
+            // });
+
+            // $(this).animate({
+            //     left: 300
+            // }, 1000, "easeInOutElastic");
+
+            $(this).toggleClass("leftTarget", "slow");
+        });
+
+    $("#draggables").children().draggable();
+
+    $("#trash").droppable({
+        activeClass: "opaque",
+        drop: function(event, ui) {
+            ui.draggable.hide("explode");
+        }
+    });
 });
